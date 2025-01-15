70faa01a3b6d3105a0aba2aa8711dbc9471bcc83
MSI <lopez212318@gmail.com>
2025-01-15 00:53:05 -0500

    main page upd + bug fixes

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,16 +1,23 @@
 import Image from "next/image";
 
 
-
 export default function main() {
   return(
   <>
-  <div className="text-2xl">
-    <h1> Hi! I&apos;m Carlos</h1>
-    <h2> I&apos;m an undergraduate student at the University of Central Florida. I am currently in my first year </h2>
+  <div className="flex justify-center mt-20">
+    <h1 className=" p-5 border-2 bg-cyan-500 rounded-xl text-6xl"> Hi! I&apos;m Carlos</h1>
   </div>
-  <div className="">
-    <Image src="/testing.jpg" width={150} height={150} className="animate-spin" alt="ok"></Image>
+  <div className="m-8 text-4xl flex justify-center">
+    <h2> I&apos;m a first year at the University of Central Florida </h2>
+  </div>
+  <div className="flex items-center justify-center"> {/* fixed im goated */}
+    <Image className ="mb-8 hover:animate-spin" src="/testing.jpg" width={150} height={200} alt="ok"></Image>
+  </div>
+  <div className="flex text-4xl justify-center">
+    <p>blah blah</p>
+  </div>
+  <div className="top-3 right-4 fixed flex justify-end"> {/* put guide in center */}
+    <Image className ="" src="/guide.png" width={200} height={200} alt="Guide Terraria"></Image>
   </div>
   </>
   );
--- src/app/(main)/projects/page.tsx
+++ src/app/(main)/projects/page.tsx
@@ -1,9 +1,33 @@
+const project = [
+    {
+        name: "The Underground",
+        description: "terraria mod using c#",
+        image: "placeholder",
+        skillsUsed: "C#",
+    },
+    {
+        name: "Spark-A-Hack",
+        description: "Hackathon project",
+        image: "placeholder",
+        skillsUsed: "React, Tailwind, Gemini API, TRPC",
+    },
+    {
+        name: "Portfolio",
+        description: "This website",
+        image: "placeholder",
+        skillsUsed: "Next.js, Tailwind",
+    },
+];
 
 
-export default function projects() {
+export default function Projects() {
     return (
         <div>
-            project
+            {project.map((proj, index) => (
+                <div key={index}>
+                    <h2>{proj.name}</h2>
+                </div>
+            ))}
         </div>
     );
 }
