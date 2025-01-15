adc6dfb852e3c0850e4fd3478de4cb01640feb91
MSI <lopez212318@gmail.com>
2025-01-14 22:41:14 -0500

    carloselopez.dev

--- src/app/(main)/components/navbar/page.tsx
+++ src/app/(main)/components/navbar/page.tsx
@@ -3,8 +3,11 @@ import Link from "next/link";
 export default function Navbar() {
     return (
        <div>
-        <nav className ="mb-10 bg-red-800 p-4">
+        <nav className ="mb-10 bg-red-800 p-4 border-b-8 border-red-400">
             <ul className = "flex space-x-56">
+                <li>
+                    <Link href="/">Home</Link>
+                </li>
                 <li>
                     <Link href="/experience">Experience</Link>
                 </li>
@@ -12,8 +15,9 @@ export default function Navbar() {
                     <Link href="/projects">Projects</Link>
                 </li>
                 <li>
-                    <Link href="/skills">Skills</Link>
+                    <Link href="/skills">Skills</Link> 
                 </li>
+                {/* Add resume + linkedin later */}
             </ul>
         </nav>
        </div> 
--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,15 +1,13 @@
-
-const carlos = {
-  name: "okok Lopez",
-  about: "Hello!",
-  age: 15,
-};
-
-export default function Home() {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold">{carlos.name}</h1>
-      <h2 className="text-xl">{carlos.about}</h2>
-    </div>
+export default function main() {
+  return(
+  <>
+  <div className="text-2xl">
+    <h1> Hi! I'm Carlos</h1>
+    <h2> I'm an undergraduate student at the University of Central Florida. I am currently in my first year </h2>
+  </div>
+  <div className="">
+    <img src="https://i.pinimg.com/736x/a7/55/89/a75589953b431144fc71348d983783bd.jpg" className="w-50 h-20 animate-spin" alt="ok"></img>
+  </div>
+  </>
   );
 }
