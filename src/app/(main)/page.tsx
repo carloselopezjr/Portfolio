02c4d52f56ce6da0b8d8820e1ad6b97c27fa1af5
MSI <lopez212318@gmail.com>
2025-01-14 23:03:42 -0500

    &apos; for apostraphies

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -6,8 +6,8 @@ export default function main() {
   return(
   <>
   <div className="text-2xl">
-    <h1> Hi! I'm Carlos</h1>
-    <h2> I'm an undergraduate student at the University of Central Florida. I am currently in my first year </h2>
+    <h1> Hi! I&apos;m Carlos</h1>
+    <h2> I&apos;m an undergraduate student at the University of Central Florida. I am currently in my first year </h2>
   </div>
   <div className="">
     <Image src="/testing.jpg" width={150} height={150} className="animate-spin" alt="ok"></Image>
