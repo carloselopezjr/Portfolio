556232ba1891038a552987e045850c8d7dc8ebbe
MSI <lopez212318@gmail.com>
2025-01-18 20:54:54 -0500

    Nav bar + font updates

--- src/app/(main)/components/navbar/page.tsx
+++ src/app/(main)/components/navbar/page.tsx
@@ -4,23 +4,26 @@ export default function Navbar() {
     return (
        <div>
         <nav className ="fixed top-0 left-0 right-0 text-creamy bg-iterationOfPurple p-6">
-            <ul className = "flex space-x-56">
-                <li>
+            <ul className = "flex space-x-32">
+                <li className ="mr-20"> 
+                    Carlos E. Lopez
+                </li>
+                <li className = "hover:underline">
                     <Link href="/">Home</Link>
                 </li>
-                <li>
+                <li className ="hover:underline">
                     <Link href="/experience">Experience</Link>
                 </li>
-                <li>
+                <li className="hover:underline">
                     <Link href="/projects">Projects</Link>
                 </li>
-                <li>
+                <li className="hover:underline">
                     <Link href="/skills">Skills</Link> 
                 </li>
-                <li>
+                <li className="hover:underline">
                     <Link href="/CS_Resume.pdf">Resume</Link>
+                    {/* text image instead of spelling out resume */}
                 </li>
-                {/* Add resume + linkedin later */}
             </ul>
         </nav>
        </div> 
--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -4,11 +4,11 @@ import Image from "next/image";
 export default function main() {
   return(
   <>
-  <div className="flex justify-center mt-20 ">
-    <h1 className="  p-5 text-creamy bg-iterationOfPurple rounded-xl text-6xl"> Hi! I&apos;m Carlos</h1>
+  <div className="flex justify-center mt-20 p-5">
+    <h1 className="  p-5 bg-iterationOfPurple rounded-xl text-6xl"> Yo... I&apos;m Carlos!</h1>
     {/* make it type and be like yo...I'm Carlos! */}
   </div>
-  <div className="text-creamy m-8 text-4xl flex justify-center">
+  <div className="m-6 text-4xl flex justify-center">
     <h2 className="rounded-xl bg-iterationOfPurple p-5"> I&apos;m a first year at the University of Central Florida </h2>
   </div>
   <div className="flex items-center justify-center"> {/* fixed im goated */}
