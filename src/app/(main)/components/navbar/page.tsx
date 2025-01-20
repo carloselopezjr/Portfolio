2c4155a7d001e7b1697e451994b023efcfb3707f
MSI <lopez212318@gmail.com>
2025-01-20 00:46:16 -0500

    navbar almost done, loving it

--- src/app/(main)/components/navbar/page.tsx
+++ src/app/(main)/components/navbar/page.tsx
@@ -1,28 +1,34 @@
 import Link from "next/link";
+import { DynamicIcon } from "lucide-react/dynamic";
 
 export default function Navbar() {
     return (
        <div>
-        <nav className =" font-semibold fixed top-0 left-0 right-0 text-creamy bg-iterationOfPurple p-6">
-            <ul className = "flex space-x-32"> {/* Make it so that when someone clicks on a different page, it remains underlined */}
-                <li className ="mr-20"> 
+        <nav className ="text-lg font-semibold fixed top-0 left-0 right-0 text-creamy bg-iterationOfPurple p-6">
+            <ul className = "flex space-x-8"> {/* Make it so that when someone clicks on a different page, it remains underlined */}
+                <li className ="mr-auto"> 
                     Carlos E. Lopez
                 </li>
-                <li className = "hover:underline">
+                <li className = "hover:opacity-70  transition-all duration-300">
                     <Link href="/">Home</Link>
                 </li>
-                <li className ="hover:underline">
-                    <Link href="/experience">Experience</Link>
-                </li>
-                <li className="hover:underline">
+                <li className="hover:opacity-70  transition-all duration-300">
                     <Link href="/projects">Projects</Link>
                 </li>
-                <li className="hover:underline">
+                <li className ="hover:opacity-70  transition-all duration-300">
+                    <Link href="/experience">Experience</Link>
+                </li>
+                <li className="hover:opacity-70  transition-all duration-300">
                     <Link href="/skills">Skills</Link> 
                 </li>
-                <li className="hover:underline">
-                    <Link href="/CS_Resume.pdf">Resume</Link>
-                    {/* text image instead of spelling out resume */}
+                <li className="hover:opacity-60 transition-all duration-300">
+                    <Link href="/CS_Resume.pdf"><DynamicIcon name="file-text"/></Link>
+                </li>
+                <li className="hover:opacity-70  transition-all duration-300">
+                    <Link href="https://github.com/carloselopezjr"><DynamicIcon name="github" /></Link>
+                </li>
+                <li className="hover:opacity-70 transition-all duration-300">
+                    <Link href="https://www.linkedin.com/in/carloselopezjr/"><DynamicIcon name="linkedin" /></Link>
                 </li>
             </ul>
         </nav>
--- src/app/(main)/skills/page.tsx
+++ src/app/(main)/skills/page.tsx
@@ -1,4 +1,3 @@
-import { platform } from "os";
 
 const languages = [
     {
