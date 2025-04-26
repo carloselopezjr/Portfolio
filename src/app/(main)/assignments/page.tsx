e5949688131ce8df269a5396bafc96d51a4683db
MSI <lopez212318@gmail.com>
2025-04-25 23:27:28 -0400

    Resume page & Instruction page done

--- src/app/(main)/assignments/page.tsx
+++ src/app/(main)/assignments/page.tsx
@@ -6,12 +6,12 @@ const assignments = [
     link: "/CS_Resume.pdf",
   },
   {
-    name: "Cover Letters",
+    name: "Cover Letter",
     link: "/cover_letter.pdf",
   },
   {
     name: "Mini-Interview",
-    link: "https://youtu.be/r6WfIsmyS7M",
+    link: "https://youtu.be/rd6WfIsmyS7M",
   },
 ];
 
@@ -19,7 +19,9 @@ const assignments = [
 {/* boxes should all be the same size fix later */}
 export default function enc() {
   return (
-    <div className="grid grid-cols-2 text-center justify-center mt-40">
+    <div>
+      <h1 className="font-bold text-2xl mt-28 text-center "> Below are redirects to my Resume, Cover Letter, and Mini-Interview! </h1>
+    <div className="mt-2 grid grid-cols-2 text-center justify-center ">
       {assignments.map((assignment, index) => (
         <div 
         key={assignment.name} 
@@ -29,11 +31,12 @@ export default function enc() {
         >
           <Link 
           href={assignment.link} 
-          className="p-20">
+          className="text-lg font-semibold p-40 hover:underline hover:opacity-70">
             {assignment.name}
           </Link>
         </div>
       ))}
+    </div>
     </div>
   );
 }
--- src/app/(main)/instructions/page.tsx
+++ src/app/(main)/instructions/page.tsx
@@ -1,9 +1,26 @@
-
-
 export default function enc() {
-    return (
-        <div className="flex justify-center mt-24">
-            hai
-        </div>
-    )
+  return (
+    <div className="mt-24">
+      <h1 className=" text-4xl font-semibold text-center "> Instructions </h1>
+      <h2 className="text-lg mt-2 opacity-90 text-center">
+        {`
+        Here are my instructions: A Beginner's Guide to Modding Terraria`}
+      </h2>
+      <h3 className="text-center text-sm opacity-90">
+        {` These instructions provide a brief understanding of how to mod Terraria
+        using tModLoader,
+        <br /> an expansion of Terraria that allows users to create their own
+        custom content `}
+      </h3>
+      <div className="flex justify-center">
+        <embed
+          className="border-4 p-4 border-purple-950 rounded-xl bg-purp"
+          src="/instructions.pdf"
+          type="application/pdf"
+          width="900px"
+          height="900px"
+        ></embed>
+      </div>
+    </div>
+  );
 }
