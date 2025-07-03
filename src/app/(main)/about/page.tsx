c38ba75749c85d9f419af61032c08534ed3b69b7
MSI <lopez212318@gmail.com>
2025-07-03 19:48:32 -0400

    Restructured About Me

--- src/app/(main)/about/page.tsx
+++ src/app/(main)/about/page.tsx
@@ -23,14 +23,6 @@ export default function About() {
     animate={{ opacity: 1, scale: 1 }}
     transition={{ duration: 0.5 }}>
       <div className="flex flex-col lg:flex-row items-center justify-center gap-8">
-        <div className="lg:w-1/2 text-white border-2 border-[#232234] backdrop-blur-md p-6 rounded-lg shadow-lg">
-          <p className="text-xl leading-relaxed">
-            {
-              "Hi, I'm Carlos, an undergraduate first-year student studying Computer Science at the University of Central Florida. I have a strong passion for software development and web design, and I enjoy creating projects that challenge my skills and expand my knowledge. Reflecting over the last year and thinking back to how I only knew Python and a little bit of HTML, I can say that I have come a long way. I have learned so much about programming and I am excited to continue learning more. In addition to programming, I also enjoy listening to music, playing games, and getting fitted up."
-            }
-          </p>
-        </div>
-
         <div className="w-[300px] h-[300px] lg:w-[400px] lg:h-[400px] relative overflow-hidden shadow-lg">
           <Image
             src="/images/me.jpg"
@@ -40,6 +32,15 @@ export default function About() {
             className="mt-4"
           />
         </div>
+        <div className="lg:w-1/2 text-white border-2 border-[#232234] backdrop-blur-md p-6 rounded-lg shadow-lg">
+          <p className="text-xl leading-relaxed">
+            {
+              "Hi, I'm Carlos, an undergraduate first-year student studying Computer Science at the University of Central Florida. I have a strong passion for software development and web design, and I enjoy creating projects that challenge my skills and expand my knowledge. Reflecting over the last year and thinking back to how I only knew Python and a little bit of HTML, I can say that I have come a long way. I have learned so much about programming and I am excited to continue learning more. In addition to programming, I also enjoy listening to music, playing games, and getting fitted up."
+            }
+          </p>
+        </div>
+
+        
       </div>
     </motion.div>
     </div>
